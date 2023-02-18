72552fb2eb67556b1ccf65363f0204c6ba016282
Demetrovichz <pugehok322@gmail.com>
2023-02-18 11:35:06 +0300

    Добавлен роутер

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
+import { Routes } from 'react-router-dom';
 import './App.css';
 import { NavMenu } from './Components/Header/NavMenu';
 function App() {
   return (
     <div className="App">
       <NavMenu />
+      <Routes></Routes>
     </div>
   );
 }
--- src/Components/Header/NavMenu.tsx
+++ src/Components/Header/NavMenu.tsx
@@ -1,12 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export function NavMenu() {
   return (
-    <nav className=" flex justify-between items-center h-[80px] px-5 bg-slate-300 text-white">
-      <h3>TodoApp</h3>
+    <nav className=" flex justify-between items-center h-[80px] px-5 bg-sky-200 text-white shadow bg-white rounded-lg">
+      <h3 className="font-bold uppercase ">TodoApp</h3>
       <span>
-        <a className="mr-10" href="/">
+        <Link className="mr-10" to="/login">
           Войти
-        </a>
-        <a href="/">Зарегистрироваться</a>
+        </Link>
+        <Link to="/register">Зарегистрироваться</Link>
       </span>
     </nav>
   );
